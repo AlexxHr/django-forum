e35c0da29ac917388b949d197faab941c2d0c079
Alexander Hristov <cawkinaa@gmail.com>
2022-04-01 20:10:55 +0300

    added static and media files

--- djangoForum/urls.py
+++ djangoForum/urls.py
@@ -1,11 +1,12 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('djangoForum.accounts.urls')),
     path('', include('djangoForum.forum.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
